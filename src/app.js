dbf29007033bf4f0132c4a70e4ca6e511572c1d4
Theo <theo.piedade@gmail.com>
2023-09-07 16:49:03 -0300

    Post complete with date empty plus 30 days

--- src/app.js
+++ src/app.js
@@ -3,24 +3,22 @@ import cors from 'cors';
 import { MongoClient, ObjectId } from "mongodb";
 import joi from 'joi';
 import dotenv from "dotenv";
-dotenv.config();
+import dayjs from 'dayjs';
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+dotenv.config();
 
 // JOI Schemas 
-
 const schema = joi.object({
     title: joi.string().required(),
-
     expireAt: joi.string().required()
 })
 
 // DB Connection 
-
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
-
 
 try {
 	await mongoClient.connect();
@@ -31,39 +29,45 @@ try {
 
 const db = mongoClient.db();
 
+// API functions
+app.post("/poll", async (req, res) => {
+    const { title, expireAt } = req.body;
 
-app.post("/poll", (req, res) => {
-    const { title, expireAt } = req.params;
+    let data = "";
+    if (expireAt && expireAt != "") data = expireAt;
+    else data = dayjs(Date.now()).add(30, 'day').format('YYYY-MM-DD HH:mm')
 
-    const validateSchema = schema.validate(req.body, { abortEarly: false })
-    if (validateSchema.error) return res.sendStatus(422);
+    const survey  = {
+        title: title,
+        expireAt: data
+    }
 
-	db.collection("survey").insertOne({
-		title: title,
-		expireAt: expireAt
-	}).then(users => res.sendStatus(201))
-		.catch(err => console.log(err.message))
+    const validateSchema = schema.validate(survey, { abortEarly: false })
+    if (validateSchema.error) return res.status(422).send(data);
+
+	
+
+        try {
+            await db.collection('surveys').insertOne(survey);
+            return res.sendStatus(201);
+         } 
+         catch (err) {
+            console.log(err);
+            res.sendStatus(500);
+        }
+      
 });
 
 
-app.get("/usuarios", (req, res) => {
-	// buscando usuários
-	db.collection("users").find().toArray()
-		.then(users => res.send(users))  // array de usuários
-		.catch(err => res.status(500).send(err.message))  // mensagem de erro
+app.get("/poll", async (req, res) => {
+    try {
+        const surveys = await db.collection('surveys').find().toArray();
+        return res.send(surveys);
+      } catch (error) {
+        console.error(error);
+        return res.sendStatus(500);
+      }
 });
-
-app.get("/usuarios/:id", (req, res) => {
-    const { id } = req.params;
-  
-    db.collection("users").findOne({ _id: new ObjectId(id) })
-          .then((data) => {
-              return res.send(data);
-          })
-          .catch(() => {
-              return res.status(500).send(err)
-          })
-  })
 
 
 const PORT = 5000;
